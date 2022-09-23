ac089b59095b727836ece02f9d9dca7d8a5c9d83
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-09-23 17:32:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ let inputSearch = '';
 let page = 1;
 let per_page = 40;
 let objectArr = {};
+let gallery = {};
 
 const messageEmptyInput = 'Sorry, write your search query.';
 const messageWrangInput = 'Sorry, there are no images matching your search query. Please try again.';
@@ -49,8 +50,9 @@ function onSearchSubmit(e) {
         return;
     }
     fetchImages();
-    setTimeout(() => { showLoadMoreBtn() }, 1000);    
-}
+    showLoadMoreBtn();   
+    }    
+
 
 async function fetchImages() {
     try {
@@ -59,25 +61,26 @@ async function fetchImages() {
             .get(`https://pixabay.com/api/?key=${API_KEY}&q=${inputSearch}&image_type=photo&orientation=horizontal
             &safesearch=true&page=${page}&per_page=${per_page}`);
 
-            const lengthArr = response.data.hits.length;
-            if (lengthArr === 0) {
-                throw new Error();
+        const lengthArr = response.data.hits.length;
+        console.log("lengthArr", lengthArr);
+        if (lengthArr === 0) {
+            throw new Error();
         }
 
         const totalHits = response.data.totalHits;
-
-        if (page === 1) {
-            Notiflix.Notify.info(`Hooray! We found ${totalHits} images`);
-        }
-        
+ 
         console.log("totalHits", totalHits);
-            objectArr = response.data.hits;
+        objectArr = response.data.hits;
         renderGalleryCard(objectArr);
-        
-            
-        let gallery = new SimpleLightbox('.gallery div a');
+        if (page === 1) {
+             Notiflix.Notify.info(`Hooray! We found ${totalHits} images`);
+        }
+        gallery = new SimpleLightbox('.gallery div a');
+        gallery.refresh();
         
         const endGallery = parseInt(totalHits / per_page) + 1;
+        console.log("endGallery", endGallery);
+        console.log("page", page);
         if (endGallery === page) {
             hideLoadMoreBtn();
             const endMsg =
